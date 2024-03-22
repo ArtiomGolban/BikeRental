9cbc45c944ccddae6704672bd9269f6f4ac9c331
Valeriu Corcodel <valeriu.corcodel@isa.utm.md>
2024-03-22 18:14:37 +0200

    Changed Login Controller

--- BikeRental.Web/Controllers/LoginController.cs
+++ BikeRental.Web/Controllers/LoginController.cs
@@ -27,16 +27,72 @@ namespace BikeRental.Web.Controllers
             return View();
         }
 
-        // POST: Login
-        public ActionResult Login(UserLogin login)
+    // GET: Register
+    public ActionResult Register()
+    {
+        return View();
+    }
+
+    public LoginController()
         {
-            if (ModelState.IsValid)
+            var bl = new BusinessLogic.BusinessLogic();
+            _session = bl.GetSessionBL();
+        }
+
+
+    // POST: Register
+    [HttpPost]
+    public ActionResult Register(ULoginData register)
+    {
+        if (ModelState.IsValid)
+        {
+            ULoginData data = new ULoginData
             {
-                ULoginData data = new ULoginData
-                {
-                    Credentials = login.Credentials,
-                    Password = login.Password
-                };
+                Username = register.Username,
+                Email = register.Email,
+                Password = register.Password
+            };
+
+            var userRegister = _session.UserRegister(data);
+
+            if (userRegister.Status)
+            {
+                // Redirect to login page after successful registration
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                ModelState.AddModelError("error", "Registration failed");
+                return View(register);
+            }
+        }
+
+     
+        return View(register);
+    }
+
+
+    // POST: Login
+    [HttpPost]
+    public ActionResult Login(UserLogin login)
+    {
+        if (ModelState.IsValid)
+
+            return View(new UserLogin());
+        }
+
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin login)
+
+        {
+            ULoginData data = new ULoginData
+            {
+
+                Username = login.Username,
+                Password = login.Password
+            };
 
                 var userLogin = _session.UserLogin(data);
 
